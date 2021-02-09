f0480cde23ec59819f6e799c42a1fc6effdc852e
shivam-kr216 <shivamkr216.com>
2021-02-09 08:37:36 +0530

    first commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ComponentsScreen = () => {
+    //const greeting = <Text style={styles.textStyle}>Hii</Text>;
+    const name = "Shivam";
+    return (
+        <View>
+            <Text style={styles.textStyle}>Getting started with react native!</Text>
+            <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 45
+    },
+    subHeaderStyle: {
+        fontSize: 20
+    }
+});
+
+export default ComponentsScreen;
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const ListScreen = () => {
+    return (
+        <View>
+            <Text>List Screens</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create (
+    {
+
+    }
+);
+
+export default ListScreen;
